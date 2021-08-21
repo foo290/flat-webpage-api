bec5151d7a15140f31f0e0ac29fc99477d9eb039
foo290 <ns290670@gmail.com>
2021-08-21 15:23:56 +0530

    allowed hosts

--- web_flat/settings.py
+++ web_flat/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hadg6daw87e2312rz6rBBR6UB87Y45W34TG293GYBEJSDJEHT'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["flatweb.herokuapp.com", '127.0.0.1', 'localhost']
 
 # Application definition
 
